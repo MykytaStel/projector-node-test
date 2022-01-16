be28ac6d2d49f818361b1ec03992e2c119d8804b
Nikita <nikitastelmashenko@gmail.com>
2022-01-16 17:27:10 +0200

    add async/await task

--- task-4.js
+++ task-4.js
@@ -1,54 +1,60 @@
-// А можно поподробнее, что надо именно тут сделать, юзать только async/await без изменения самого файла db? или же тут надо просто поубирать вложенность
-// немного не понял задачу :(
-// ниже я не стал доделывать, решил уточнить
-
 import { database } from "./db";
 
-const buyBookForUser = async (bookId, userId, callback) => {
-  const { getUser, getUsersBook, buyBook } = database;
-
-  await getUser(userId, async (err, user) => {
-    try {
-      await getUsersBook(user?.id, (err, userBooks) => {
-        if (userBooks?.includes(bookId)) {
-          callback(`User already has book with id=${bookId}`);
-        } else {
-          buyBook(bookId, (err) => {
-            if (err) {
-              callback(err);
-            } else {
-              callback(null, "Success");
-            }
-          });
-        }
-      });
-    } catch (e) {
-      return callback(e);
-    }
+function promisifyCallback(id, callBack) {
+  return new Promise(function (resolve, reject) {
+    callBack(id, function (err, data) {
+      if (err !== null) {
+        reject(err);
+      }
+      resolve(data);
+    });
   });
+}
+
+const buyBookForUser = async (bookId, userId, callback) => {
+  const { buyBook, getUser, getUsersBook } = database;
+
+  try {
+    await promisifyCallback(userId, getUser);
+
+    const userBooks = await promisifyCallback(userId, getUsersBook);
+
+    if (userBooks?.includes(bookId)) {
+      return callback(`User already has book with id=${bookId}`);
+    }
+
+    try {
+      await promisifyCallback(bookId, buyBook);
+      callback(null, "Successs");
+    } catch (e) {
+      callback(e);
+    }
+  } catch (e) {
+    callback(e);
+  }
 };
 
 buyBookForUser(1, 1, (err, message) => {
-  console.log("err", err); // null
+  console.log("err-01", err); // null
   console.log("message", message); // 'Success'
 });
 
 buyBookForUser(1, 2, (err, message) => {
-  console.log("err = 2", err); // 'User already has book with id=1'
+  console.log("err = 22", err); // 'User already has book with id=1'
   console.log("message = 2", message); // undefined
 });
 
 buyBookForUser(3, 2, (err, message) => {
-  console.log(err); // null
-  console.log(message); // 'Success'
+  console.log("err = 3", err); // null
+  console.log("message = 3", message); // 'Success'
 });
 
 buyBookForUser(5, 2, (err, message) => {
-  console.log(err); // 'Book with id=5 not found'
-  console.log(message); // undefined
+  console.log("4", err); // 'Book with id=5 not found'
+  console.log("m4", message); // undefined
 });
 
 buyBookForUser(1, 3, (err, message) => {
-  console.log(err); // 'User with id=3 not found'
-  console.log(message); // undefined
+  console.log("m5", err); // 'User with id=3 not found'
+  console.log("m5", message); // undefined
 });
